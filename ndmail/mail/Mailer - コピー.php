ff40e454ad5675f3d6d4c84f8056df8bcfd1dade
murayamatoru <murayama@bronze.ocn.ne.jp>
2026-03-01 12:09:20 +0900

    サイトからtoiawase@fk-nextdesign～を除去した

--- ndmail/mail/Mailer - コピー.php
+++ ndmail/mail/Mailer - コピー.php
@@ -10,26 +10,6 @@ class Mailer{
     
     //メールを送信する
     public function sendInfo($mailAddress, $mailMessage = "", $subject = ""){
-        $mail = new Qdmail();
-        $mail -> errorDisplay( false );
-        $mail -> smtp( true );
-        $param = array(
-            'host' => 'fk-nextdesign.sakura.ne.jp',
-            'port' => 587,
-            'from' => 'toiawase@fk-nextdesign.sakura.ne.jp',
-            'protocol' => 'SMTP_AUTH',
-            'user' => 'toiawase',
-            'pass' => 'next3148'
-        );
-        $mail -> smtpServer($param);
-        $mail->to($mailAddress);
-        $mail->from('toiawase@fk-nextdesign.sakura.ne.jp', Constants::SITE_NAME . ' フォームメール');
-        $mail->subject($subject);
-        $mailText = "フォームメール\n";
-        $mailText .= $mailMessage;
-        $mail->text($mailText);
-        $resut = $mail->send();
-        //$this->sendLog($mailAddress, "問合せあり"); //ログ
         return $resut;
     }
 }
